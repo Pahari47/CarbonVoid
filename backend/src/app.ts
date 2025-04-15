91f3239ee5a1629e9212e15a3322dd61039755ec
Pahari47 <nachiketapahari00@gmail.com>
2025-04-15 21:31:21 +0530

    backend setup done

---
+++ backend/src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+export default app;
